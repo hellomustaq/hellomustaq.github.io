d789e9ebcc4ba88c198d2086db0c84696f16b726
Mustaque Ahemmed <hellomstq@gmail.com>
2017-05-17 00:22:40 +0600

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,13 @@
+function tagLineShow(){
+    document.getElementById('tagLine').style.visibility="visible";
+    document.getElementById('tagLine').style.display="block";
+}
+function tagLineOff(){
+    document.getElementById('tagLine').style.visibility="hidden";
+    document.getElementById('tagLine').style.display="none";
+}
+
+/*function tagStable(){
+    document.getElementById('tag').style.visibility="visible"
+    setInterval(function() {document.getElementById('tag').style.visibility="hidden";},3000);
+}*/
